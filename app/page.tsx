d21bc1522744893ddf35f503bf2e26b4c27eff9e
Gustavo <gondo.g@aol.com>
2026-02-11 23:02:43 -0300

    adicionado a page portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Header from "./Components/Header";
 
 export default function Home() {
@@ -9,10 +10,10 @@ export default function Home() {
         <h1 className="text-6xl"> Bem-vindo ao meu <span className="text-blue-600">portfólio</span></h1>
         <p className="text-2xl mt-3">Comece a construir seu portfólio hoje!</p>
         <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <div className="p-6 mt-6 text-left border rounded-2xl hover:text-blue-600 focus:text-blue-600">
+          <Link href="/portfolio" className="p-6 mt-6 text-left border rounded-2xl hover:text-blue-600 focus:text-blue-600">
             <h2 className="text-2xl font-semibold"> Ver portfólio</h2>
             <p className="mt-4 text-xl">Explore os trabalhos realizados</p>
-          </div>   
+          </Link>  
           <div className="p-6 mt-6 text-left border rounded-2xl hover:text-blue-600 focus:text-blue-600">
             <h2 className="text-2xl font-semibold">Área Administrativa</h2>
             <p className="mt-4 text-xl">Gerencie o seu portfólio (requer login)</p>
